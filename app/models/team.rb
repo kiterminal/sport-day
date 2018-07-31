415b0467cb8422c436fbfca10fef33697a052fc3
Chainarin Thanichaiyawat <chainarin.thanichaiyawat@scb.co.th>
2018-08-01 01:53:24 +0700

    Remove active record

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,14 +1,25 @@
-class Team < ApplicationRecord
+class Team
+  include Mongoid::Document
+
+  field :first_name, type: String
+  field :last_name, type: String
+  field :employee_id, type: String
+  field :color, type: String
+
+  auto_increment :sequence, collection: :teams
+
   validates :first_name, presence: true
   validates :last_name, presence: true, uniqueness: { scope: :first_name }
-  validates :code, presence: true, uniqueness: true
+  validates :employee_id, presence: true, uniqueness: true
 
   after_create :random_color
+
+  default_scope { order('first_name asc') }
 
   private
 
   def random_color
-    color = self.id % 4
+    color = self.sequence % 4
     self.color = color > 0 ? color : 4
     save
   end
